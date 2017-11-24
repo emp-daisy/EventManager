679a9f0806a6fd9887616a15c3d5cff442d955a1
emp-daisy <adajess01@gmail.com>
2017-11-24 10:57:24 +0100

    eslint error fixed and removed unused import

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express(); // init express
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 //= ==================ROUTER=================
 app.use('/v1/', centerRouter);
@@ -19,17 +19,17 @@ app.use('/v1/', eventRouter);
 
 //= ======== DEFAULT ROUTE==========
 app.use((req, res) => {
-    // Invalid request
-    res.status(404).json({
-        error: {
-            name: 'Error',
-            message: 'Invalid URL Request'
-        }
-    });
+  // Invalid request
+  res.status(404).json({
+    error: {
+      name: 'Error',
+      message: 'Invalid URL Request'
+    }
+  });
 });
 
 
 app.listen(port, () => {
-    console.log('Server running on port', port);
+  console.log('Server running on port', port);
 });
 export default app;
