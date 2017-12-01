3a670982093c0efc1fd442f1b6817a2182195172
emp-daisy <adajess01@gmail.com>
2017-12-01 12:23:16 +0100

    test env var

--- server/models/index.js
+++ server/models/index.js
@@ -8,7 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = Config[env];
 const db = {};
 let sequelize;
-
+console.log('CONFIGURATION', config);
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
