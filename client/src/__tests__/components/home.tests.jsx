4691e6c031d73cec4842efdba4fd51da74858891
Jessica M <adajess01@gmail.com>
2018-06-01 10:03:13 +0100

    [Feature]: Add tests for homepage
    Setup front-end test
    Add test for home page
    
    [Finishes #157913534]

---
+++ client/src/__tests__/components/home.tests.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import ConnectedHomePage, { HomePage } from '../../components/home';
+
+const mockStore = configureStore();
+const history = {
+  push: jest.fn()
+};
+const mockProps = {
+  listOfCenters: [],
+  getCenters: jest.fn(),
+  history
+};
+
+describe('HomePage Component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<HomePage {...mockProps} history={history} />);
+  });
+
+  describe('Default components', () => {
+    it('should render successful', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render successful with items in centerList', () => {
+      mockProps.listOfCenters = [{
+        id: 2, image: undefined, name: 'Test block', location: 'Jest'
+      }];
+      expect(wrapper).toMatchSnapshot();
+    });
+    it('should render successful with items in centerList including image', () => {
+      mockProps.listOfCenters = [{
+        id: 2, image: 'image', name: 'Test block', location: 'Jest'
+      }];
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should run onCenterPageNavigate function', () => {
+      wrapper.instance().onCenterPageNavigate();
+      expect(mockProps.history.push).toHaveBeenCalled();
+    });
+  });
+
+  describe('Connected HomePagecomponent', () => {
+    it('should render connected component', () => {
+      const store = mockStore({
+        center: {
+          allCenterList: []
+        }
+      });
+      wrapper = shallow(<ConnectedHomePage store={store} history={history} />);
+      expect(wrapper.length).toBe(1);
+    });
+  });
+});
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -6,7 +6,6 @@ import Footer from './footer';
 import { Header } from './header';
 import CardBlock from './cards';
 import { getCenters } from '../actions/center';
-import history from '../actions/history';
 /**
  *
  *
@@ -30,17 +29,15 @@ export class HomePage extends Component {
    * @memberof HomePage
    */
   componentWillMount() {
-    this
-      .props
-      .getCenters();
+    this.props.getCenters();
   }
   /**
- *
- * @returns{null} no return
- * @memberof HomePage
- */
+   *
+   * @returns{null} no return
+   * @memberof HomePage
+   */
   onCenterPageNavigate() {
-    history.push(this.state.centerUrl);
+    this.props.history.push(this.state.centerUrl);
   }
   /**
    *
@@ -54,58 +51,58 @@ export class HomePage extends Component {
     return (
       <div className="wrapper" id="wrapper">
         <section className="intro parallex-img" id="intro">
-          <div className="d-flex flex-column" >
+          <div className="d-flex flex-column">
             <Header />
             <div className="jumbotron home m-auto" id="banner">
               <div className="container-fluid text-center">
                 <h2 className="d-none d-md-block display-2 font-weight-bold">
-              Book your events at the speed of thought
+                  Book your events at the speed of thought
                 </h2>
                 <h1 className="d-md-none font-weight-bold text-white">
-              Book your events at the speed of thought
+                  Book your events at the speed of thought
                 </h1>
                 <h3 className="sub-banner font-italics">
-                    Browse through the available events and showcase your events.
-                    <br />
+                  Browse through the available events and showcase your events.
+                  <br />
                   <span className="font-italic">
-                      Let us help make your event come alive
+                    Let us help make your event come alive
                   </span>
                 </h3>
               </div>
             </div>
           </div>
         </section>
-        <section
-          className="latest parallex-img"
-          id="latest"
-        >
+        <section className="latest parallex-img" id="latest">
           <h3 className="text-white p-2">Latest Centers</h3>
-          {listOfCenters.length > 0 &&
-          <div className="row justify-content-center m-2">
-            {listOfCenters.slice(0, 6).map(center => (
-              <div className="col-sm-6 col-md-4 col-lg-3 mt-2" key={center.id}>
-                <CardBlock
-                  id={center.id}
-                  src={center.image
-                  ? center.image
-                  : undefined}
-                  title={center.name}
+          {listOfCenters.length > 0 && (
+            <div className="row justify-content-center m-2">
+              {listOfCenters.slice(0, 6).map(center => (
+                <div
+                  className="col-sm-6 col-md-4 col-lg-3 mt-2"
+                  key={center.id}
+                >
+                  <CardBlock
+                    id={center.id}
+                    src={center.image ? center.image : undefined}
+                    title={center.name}
+                    onClick={this.onCenterPageNavigate}
+                    buttonText="View"
+                  >
+                    {center.location}
+                  </CardBlock>
+                </div>
+              ))}
+              <div className="d-flex align-items-center">
+                <button
+                  className="btn btn-dark btn-lg border-1 text-white py-3"
                   onClick={this.onCenterPageNavigate}
-                  buttonText="View"
-                >{center.location}
-                </CardBlock>
+                >
+                  Explore Centers <br />
+                  <span className="fa fa-arrow-circle-o-right" />
+                </button>
               </div>
-            ))}
-            <div className="d-flex align-items-center" >
-              <button
-                className="btn btn-dark btn-lg border-1 text-white py-3"
-                onClick={this.onCenterPageNavigate}
-              >Explore Centers
-              <br />
-                <span className="fa fa-arrow-circle-o-right" />
-              </button>
             </div>
-          </div>}
+          )}
         </section>
         <Footer />
       </div>
@@ -113,13 +110,21 @@ export class HomePage extends Component {
   }
 }
 
-const mapStateToProps = state => ({ listOfCenters: state.center.allCenterList });
-const matchDispatchToProps = dispatch => bindActionCreators({
-  getCenters
-}, dispatch);
+const mapStateToProps = state => ({
+  listOfCenters: state.center.allCenterList
+});
+const matchDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getCenters
+    },
+    dispatch
+  );
 
 HomePage.propTypes = {
   listOfCenters: PropTypes.arrayOf(PropTypes.object).isRequired,
-  getCenters: PropTypes.func.isRequired
+  getCenters: PropTypes.func.isRequired,
+  history: PropTypes.objectOf(PropTypes.any).isRequired
 };
+
 export default connect(mapStateToProps, matchDispatchToProps)(HomePage);
